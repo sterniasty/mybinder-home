e3a2c4e864d6b7af0a5ad1db2e85ef8aa261ff5f
Lukasz Sternowski <sterna@tlen.pl>
2026-04-17 04:47:20 +0200

    Add /bb rewrite to Bloodborne assistant

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
     defaultLocale: "en",
     localeDetection: false,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/bb',
+        destination: 'https://bb-app-eight.vercel.app/bb',
+      },
+      {
+        source: '/bb/:path*',
+        destination: 'https://bb-app-eight.vercel.app/bb/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
